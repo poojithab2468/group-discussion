e7abbc46f92f4b0a5d34fec891c2778c040e4093
poojithab <poojithab2468@gmail.com>
2026-02-24 12:30:52 +0530

    Update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -22,7 +22,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="(home)"
+        name="dashboard"
         options={{
           title: "Practice",
           tabBarIcon: ({ color, size }) => <MessageSquare size={size} color={color} />,
